0aa32bbd3514c564fd4b54a34cf54b20a2f32f00
Oscar Luu <oscar.luu@bit.ly>
2019-10-23 13:20:10 -0600

    handler: regex match for links

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
+	"github.com/OscarLuu/bitlybot/lib"
+	"github.com/bwmarrin/discordgo"
+)
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,4 +22,30 @@ import "github.com/OscarLuu/bitlybot/cmd"
 
 func main() {
 	cmd.Execute()
+	// initialize discordgo bot session
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session ", err)
+		return
+	}
+
+	dg.AddHandler(lib.OnMessageCreate)
+
+	// open connection to Discord
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error with connection ", err)
+		return
+	}
+
+	// allow control c termination
+	fmt.Println("Running, awaiting CTRL-C to turn shutdown.")
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs
+	fmt.Println("Exiting")
+
+	// Close connection
+	dg.Close()
 }
--- pkg/handler/main.go
+++ pkg/handler/main.go
@@ -1,21 +1,33 @@
 package handler
 
 import (
-	"strings"
+	"regexp"
 
 	"github.com/bwmarrin/discordgo"
 )
 
-// MessageCreate checks for new messages that are created
-func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if strings.Contains(m.Content, "hello") {
-		MessageDelete(s, m.ChannelID, m.ID)
-		s.ChannelMessageSend(m.ChannelID, "Received")
+// OnMessageCreate checks for new messages that are created
+func OnMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Checks if the message creator is the same as the bot
+	// if it is the same then ignore it
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// regex match for anything that starts with http
+	// and has at least one . in it
+	re := regexp.MustCompile(`^http*\.*`)
+	if re.MatchString(m.Content) {
+		s.ChannelMessageSend(m.ChannelID, m.Content)
+		// this is where we want to call bitly api and pass the link as string
+		// should return here and have channel message send the short link
+		// calls ChannelMessageDelete to delete the users link
+		// append the username of the poster to the message send
 	}
 }
 
 // MessageDelete deletes message
 func MessageDelete(s *discordgo.Session, chanID string, mID string) {
+	// this function is designed to delete the link message
 	s.ChannelMessageDelete(chanID, mID)
-	s.ChannelMessageSend(chanID, "Hit message delete function")
 }
