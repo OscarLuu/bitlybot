b9edbc5b1739245f71ddb9bad426af437a552189
Oscar Luu <oscar.luu@bit.ly>
2019-10-22 18:10:18 -0600

    test: testing return

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -25,4 +28,38 @@ func main() {
 		return
 	}
 
+	dg.AddHandler(messageCreate)
+
+	// open connection to Discord
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error with connection ", err)
+		return
+	}
+
+	// allow control c termination
+	fmt.Println("Running, awaiting CTRL-C to turn shutdown.")
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs
+	fmt.Println("Exiting")
+
+	// Close connection
+	dg.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is ping reply with pong
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong")
+	}
+
+	// If the message is pong reply with ping"
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping")
+	}
 }
